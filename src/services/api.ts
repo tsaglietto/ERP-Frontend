8551b4987ea8b0e6f03bf5b3f14631ad3fc8c7da
tsaglietto <tsaglietto@gmail.com>
2026-05-27 15:09:46 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://erp-agropecuaria-production.up.railway.app';
 
 const api = axios.create({ baseURL: API_URL });
 
